8f9c5dc3ed29c92ba8590ee10628162d8cd205e3
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-02-17 12:56:26 +0600

    Class-39

---
+++ script.js
@@ -0,0 +1,52 @@
+function sum(x, y)
+{
+    let digit1 = x;
+    let digit2 = y;
+
+    let result = digit1+digit2;
+
+    console.log("The sum is = "+result);
+}
+
+
+
+
+
+function sub(digit1, digit2)
+{
+    let number1 = digit1;
+    let number2 = digit2;
+
+    let result = number1-number2;
+
+    console.log("The sub is = "+result);
+}
+
+function multiplication(a, b)
+{
+    let number1 = a;
+    let number2 = b;
+
+    let result = a*b;
+
+    console.log("The multiplication is = "+result);
+}
+function divide(m,n )
+{
+    let number1 = m;
+    let number2 = n;
+
+    let result = m/n;
+
+    console.log("The divide is = "+result);
+}
+
+
+sum(50, 50);
+
+sub(60, 30);
+
+multiplication(9, 9);
+
+divide(81,9)
+
